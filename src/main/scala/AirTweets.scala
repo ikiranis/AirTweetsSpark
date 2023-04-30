0b265267796f177513d9effa1be0d3a9976d2ed3
rocean <rocean74@gmail.com>
2023-05-01 01:34:17 +0300

    Load csv

--- src/main/scala/AirTweets.scala
+++ src/main/scala/AirTweets.scala
@@ -1,18 +1,28 @@
-import org.apache.spark.SparkContext._
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 
 object AirTweets {
     def main(args: Array[String]): Unit = {
-        val sparkConf = new SparkConf().setMaster("local").setAppName("AirTweet")
-        val sc = new SparkContext(sparkConf) // create spark context
+        val ss = SparkSession
+            .builder()
+            .appName("AirTweets")
+            .config("spark.master", "local")
+            .getOrCreate()
 
-        val inputFile = "input/tweets.txt"
+        val inputFile = "input/tweets.csv"
         val outputDir = "output"
 
+        val df = ss.read.option("header", "true").csv(inputFile)
+
+        // Show the first few lines of the DataFrame
+        df.show()
+
+        // Print the schema of the DataFrame
+        df.printSchema()
+
         // Διάβασμα του αρχείου και αφαίρεση σημείων στίξης. Μετατροπή όλων των χαρακτήρων σε πεζά.
-        val txtFile = sc.textFile(inputFile)
-            .map(_.replaceAll("[^A-Za-z0-9]+", " "))
-            .map(_.toLowerCase)
+//        val txtFile = ss.textFile(inputFile)
+//            .map(_.replaceAll("[^A-Za-z0-9]+", " "))
+//            .map(_.toLowerCase)
     }
 
 }
